b0c42eae98365e6aa753028452f175f0d350843b
lindani <lindani@projectcodex.co>
2016-01-20 13:01:37 +0200

    Added downvote button that do the opposite of the upvote button

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,16 @@ Rails.application.routes.draw do
   resources :topics do
     member do
       post 'upvote'
+      delete 'downvote'
     end
   end
+
+  # resources :topics do
+  #   member do
+      
+  #   end
+  # end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
