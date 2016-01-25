d3ed6960428b0b75ec89c0bbae4f2b9ecbc6d07b
lindani <lindani@projectcodex.co>
2016-01-25 09:36:59 +0200

    installed gem file for table dispaly

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require dataTables/jquery.dataTables
+//= require dataTables/extras/dataTables.responsive
 //= require turbolinks
 //= require_tree .
+
