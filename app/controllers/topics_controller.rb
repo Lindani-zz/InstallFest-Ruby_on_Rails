714f22b292cd33d0ca1a2b54bd6f6c0d3d80a7c1
lindani <lindani@projectcodex.co>
2016-01-25 08:58:34 +0200

    Delete sort query and added sorting method in topic model

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,7 +4,7 @@ class TopicsController < ApplicationController
   # GET /topics
   # GET /topics.json
   def index
-    @topics = Topic.all.sorted_topics
+    @topics = Topic.sorted_topics
   end
 
   # GET /topics/1
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,26 +2,8 @@ class Topic < ActiveRecord::Base
   has_many :votes, dependent: :destroy
 
 	def self.sorted_topics
-	  # There are ways to do this with database agnostic syntax, but the resulting
-	  # Ruby/Rails code is rather overwhelming
-	  find_by_sql(
-    	"SELECT topics.*,
-	      (SELECT count(votes.topic_id) FROM votes WHERE votes.topic_id = topics.id)
-	        AS votes_count
-	      FROM topics
-	      ORDER BY votes_count desc"
-	    )
-	end
-
-  	def vote_count
-	    # In the above SQL there is the line that counts the votes and creates the
-	    # selected query column "votes_count", which means that each Topic has a
-	    # "votes_count" attribute.
-	    #
-	    # If we have the attribute, use it, otherwise compute the attribute. For
-	    # this to truly be effective, in the index page, replace "topic.votes.count"
-	    # with "topic.vote_count"
-	    attributes['votes_count'] || votes.count
+	  # Sorting topics by their number of votes
+	    Topic.all.sort_by { |topic| topic.votes.count }.reverse
 	end
 
 end
